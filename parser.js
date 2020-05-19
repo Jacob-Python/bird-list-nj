8fa189a9d51686528a24891a4669fc0b77fe1032
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-05-19 19:06:23 -0400

    Added search

--- parser.js
+++ parser.js
@@ -7,49 +7,50 @@ function get_bird_by_name(name){
 	let f = 0;
 	let fi = 0;
 	for (let i = 0; i < json.length; i ++){
-		if (json[i].name == name){
+		if (json[i].name.toLowerCase() == name){
 			f = 1;
 			fi = i;
 		}
 	}
 	if (f == 0){
-		return Result(json[fi]);
+		return json[fi]
 	} else {
 		return "Bird not found."
 	}
 }
 function get_bird_by_index(index){
 	try {
-		return Result(json[index]);
+		return json[index]
 	} catch(e) {
 		return "Bird not found."
 	}
 }
 function search(args){
 	let keyword = args.keyword;
+	let match_cat = args.searchType;
+	let order = args.order;
 	let matches = [];
-}
-class Result{
-	constructor(json){
-		this.json = json;
-	}
-	html(){
-		let htmlval = [];
-		for (var i = 0; i < this.json.length; i ++){
-			htmlval.append("<h3>"+this.json[i].name+"</h3><p>Family: "+this.json[i].type[0]+"s / "+this.json[i].type[0]+"</p>")
+	for (let item of json){
+		let count;
+		if (match_cat == "name"){
+			itemtext = item.name;
+			count = (itemtext.match(RegExp(keyword, 'g')) || []).length;
+		} else {
+			itemtext = item.type[0];
+			let count1 = (itemtext.match(RegExp(keyword, 'g')) || []).length;
+			itemtext = item.type[1];
+			let count2 = (itemtext.match(RegExp(keyword, 'g')) || []).length;
+			count = count1 + count2 / 2;
 		}
-		return html;
+		matches.push(count)
 	}
-	csv(){
-		var json = this.json;
-		var fields = Object.keys(json[0])
-		var replacer = function(key, value) { return value === null ? '' : value } 
-		var csv = json.map(function(row){
-		  return fields.map(function(fieldName){
-		    return JSON.stringify(row[fieldName], replacer)
-		  }).join(',')
-		})
-		csv.unshift(fields.join(',')) // add header column
-		return csv.join('\r\n');
+	matches = matches.sort();
+	if (!order) {
+		let temp = [];
+		for (var i = matches.length; i > 0; i --){
+			temp.push(matches[i])
+		}
+		matches = temp;
 	}
+	return matches;
 }
