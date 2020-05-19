a44befcd5e1771a7dddb13f7b6ce257d2223c0ea
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-05-19 18:18:03 -0400

    Added CSV and HTML convert

--- parser.js
+++ parser.js
@@ -13,15 +13,43 @@ function get_bird_by_name(name){
 		}
 	}
 	if (f == 0){
-		return json[fi];
+		return Result(json[fi]);
 	} else {
-		console.error("No such bird")
+		return "Bird not found."
 	}
 }
 function get_bird_by_index(index){
 	try {
-		return json[index];
+		return Result(json[index]);
 	} catch(e) {
-		console.error("No such bird")
+		return "Bird not found."
+	}
+}
+function search(args){
+	let keyword = args.keyword;
+	let matches = [];
+}
+class Result{
+	constructor(json){
+		this.json = json;
+	}
+	html(){
+		let htmlval = [];
+		for (var i = 0; i < this.json.length; i ++){
+			htmlval.append("<h3>"+this.json[i].name+"</h3><p>Family: "+this.json[i].type[0]+"s / "+this.json[i].type[0]+"</p>")
+		}
+		return html;
+	}
+	csv(){
+		var json = this.json;
+		var fields = Object.keys(json[0])
+		var replacer = function(key, value) { return value === null ? '' : value } 
+		var csv = json.map(function(row){
+		  return fields.map(function(fieldName){
+		    return JSON.stringify(row[fieldName], replacer)
+		  }).join(',')
+		})
+		csv.unshift(fields.join(',')) // add header column
+		return csv.join('\r\n');
 	}
 }
